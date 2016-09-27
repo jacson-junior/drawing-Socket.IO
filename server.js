1a37a0d1d0f4bf08364f895fe960cfc317029091
Jacsongsj <jacson.g.junior@gmail.com>
2016-09-26 22:48:51 -0300

    port fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express'),
 
 var server = http.createServer(app);
 var io = io.listen(server);
-server.listen(8008);
+server.listen(process.env.PORT || 8008);
 app.use(express.static(__dirname + '/public'));
 app.use('/', express.static(__dirname + '/www')); 
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); 
@@ -14,7 +14,6 @@ app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap-colorpicker/dist/css'));
 app.use('/img', express.static(__dirname + '/node_modules/bootstrap-colorpicker/dist/img')); 
-console.log("Server : 127.0.0.1:8008");
 
 var line_history = [];
 
